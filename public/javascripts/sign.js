e15201ebb6bda7c167bccc20a6761538d2bde45e
ljp <3313014826@qq.com>
2019-01-17 15:58:54 +0800

    备份

--- public/javascripts/sign.js
+++ public/javascripts/sign.js
@@ -2,6 +2,7 @@ function check(){
     let psd1=document.getElementById("psd1").value;
     let psd2=document.getElementById("psd2").value;
     if(psd1!=psd2){
+        // alert("密码不相等");
         document.getElementsByTagName('form').item(0).innerHTML+="<div>密码不相等</div>"
     }
 }
